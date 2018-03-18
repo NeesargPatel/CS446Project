c3dfc2416a575909d11aa1ad9452b56f9fae7fb6
vigneshwaran sendhil <vsendhil@uwaterloo.ca>
2018-03-17 20:38:11 -0400

    Added help pages for new users

---
+++ app/src/main/java/cs446/cs/uw/tictacwoah/activities/HelpActivity.java
@@ -0,0 +1,110 @@
+package cs446.cs.uw.tictacwoah.activities;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import cs446.cs.uw.tictacwoah.R;
+
+/**
+ * Created by vicdragon on 2018-03-17.
+ */
+
+public class HelpActivity extends AppCompatActivity {
+    
+    private static final int PAGES = 4;
+
+    private ViewPager pager;
+    private PagerAdapter pageAdapter;
+
+    private int displayWidth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_pager);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+
+        pager = findViewById(R.id.help_pager);
+        pageAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), this);
+        pager.setAdapter(pageAdapter);
+        pager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                invalidateOptionsMenu();
+            }
+        });
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        displayWidth = displayMetrics.widthPixels;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.help_menu, menu);
+
+        menu.findItem(R.id.action_previous).setEnabled(pager.getCurrentItem() > 0);
+
+        MenuItem item = menu.add(Menu.NONE, R.id.action_next, Menu.NONE,
+                (pager.getCurrentItem() == pageAdapter.getCount() - 1)
+                        ? R.string.action_finish
+                        : R.string.action_next);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            /*case android.R.id.home:
+                NavUtils.navigateUpTo(this, new Intent(this, HomeActivity.class));
+                return true;*/
+
+            case R.id.action_previous:
+                pager.setCurrentItem(pager.getCurrentItem() - 1);
+                return true;
+
+            case R.id.action_next:
+                pager.setCurrentItem(pager.getCurrentItem() + 1);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        private Context context;
+        ScreenSlidePagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return HelpFragment.create(position, displayWidth, context);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGES;
+        }
+    }
+}
---
+++ app/src/main/java/cs446/cs/uw/tictacwoah/activities/HelpFragment.java
@@ -0,0 +1,123 @@
+package cs446.cs.uw.tictacwoah.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.sax.RootElement;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import cs446.cs.uw.tictacwoah.R;
+import cs446.cs.uw.tictacwoah.models.Board;
+import cs446.cs.uw.tictacwoah.models.Piece;
+import cs446.cs.uw.tictacwoah.views.BoardView;
+import cs446.cs.uw.tictacwoah.views.piece.PieceView;
+
+/**
+ * Created by vicdragon on 2018-03-17.
+ */
+
+public class HelpFragment extends Fragment {
+    public static final String ARG_PAGE = "page";
+    public static final String DISPLAY_WIDTH = "width";
+    public static final String CONTEXT = "context";
+
+    private int pageNum, displayWidth;
+    private Context context;
+
+    public static HelpFragment create(int pageNumber, int displayWidth, Context context) {
+        HelpFragment fragment = new HelpFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, pageNumber);
+        args.putInt(DISPLAY_WIDTH, displayWidth);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public HelpFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNum = getArguments().getInt(ARG_PAGE);
+        displayWidth = getArguments().getInt(DISPLAY_WIDTH);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup) inflater
+                .inflate(R.layout.activity_help, container, false);
+
+        TextView text = view.findViewById(R.id.help_text);
+        int num = this.getPageNumber();
+        if (num == 0)
+            text.setText(R.string.help_text_page1);
+        else {
+            float cellWidth = displayWidth / Board.SIZE;
+            BoardView boardView = new BoardView(getActivity(), 500, cellWidth);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+            );
+            boardView.setLayoutParams(layoutParams);
+            RelativeLayout layout = view.findViewById(R.id.help_layout);
+
+            if (num == 1) {
+                text.setText(R.string.help_text_page2);
+                layout.addView(boardView);
+                Piece piece1 = new Piece(0, 0, 1, 1);
+                Piece piece2 = new Piece(0, 1, 1, 1);
+                Piece piece3 = new Piece(0, 2, 1, 1);
+                drawPieceOnBoard(piece1, boardView, layout);
+                drawPieceOnBoard(piece2, boardView, layout);
+                drawPieceOnBoard(piece3, boardView, layout);
+            }
+            else if (num == 2) {
+                text.setText(R.string.help_text_page3);
+                layout.addView(boardView);
+                Piece piece1 = new Piece(0, 2, 0, 1);
+                Piece piece2 = new Piece(0, 2, 1, 1);
+                Piece piece3 = new Piece(0, 2, 2, 1);
+                drawPieceOnBoard(piece1, boardView, layout);
+                drawPieceOnBoard(piece2, boardView, layout);
+                drawPieceOnBoard(piece3, boardView, layout);
+            }
+            else if (num == 3) {
+                text.setText(R.string.help_text_page4);
+                layout.addView(boardView);
+                Piece piece1 = new Piece(0, 0, 1, 0);
+                Piece piece2 = new Piece(0, 1, 1, 1);
+                Piece piece3 = new Piece(0, 2, 1, 2);
+                drawPieceOnBoard(piece1, boardView, layout);
+                drawPieceOnBoard(piece2, boardView, layout);
+                drawPieceOnBoard(piece3, boardView, layout);
+            }
+        }
+        return view;
+    }
+
+    public int getPageNumber() {
+        return pageNum;
+    }
+
+    private void drawPieceOnBoard(Piece piece, BoardView boardView, RelativeLayout rootLayout){
+        int sizeId = piece.getSizeId();
+        int rowId = piece.getRowId();
+        int colId = piece.getColId();
+        float cellWidth = boardView.getCellWidth();
+
+        float offset = (cellWidth - PieceView.SIZES[sizeId]) / 2;
+        float x = cellWidth * rowId + offset;
+        float y = boardView.MARGIN_TOP + cellWidth * colId + offset;
+
+        PieceView view = PieceView.getPieceView(getActivity(), (int)x, (int)y,
+                PieceView.SIZES[sizeId], PieceView.COLORS[piece.getId()], PieceView.SHAPE.CIRCLE);
+        rootLayout.addView(view);
+        view.startAnimation();
+    }
+}
