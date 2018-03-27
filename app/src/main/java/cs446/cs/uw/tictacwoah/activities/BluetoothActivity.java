db18672cd5e53e657a8a09b8289d5db6e0a61258
vigneshwaran sendhil <vsendhil@uwaterloo.ca>
2018-03-26 20:54:33 -0400

    improved UI where navigation panel will disappear after selecting an option in dialogs.

--- app/src/main/java/cs446/cs/uw/tictacwoah/activities/BluetoothActivity.java
+++ app/src/main/java/cs446/cs/uw/tictacwoah/activities/BluetoothActivity.java
@@ -23,6 +23,7 @@ public class BluetoothActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -31,7 +32,7 @@ public class BluetoothActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
-        setContentView(R.layout.activity_bluetooth);
+
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -47,6 +48,19 @@ public class BluetoothActivity extends AppCompatActivity {
                 onClickBtn(v, false);
             }
         });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 
     public void onClickBtn(View view, Boolean isHost) {
